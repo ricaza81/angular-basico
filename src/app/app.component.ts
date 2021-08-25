a0b2a3ab7de71cd3b24f047ad42c8f3f53706fc5
ricaza81 <ricaza81@gmail.com>
2021-08-24 23:13:38 -0500

    angular-basico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular2021';
+  //title = 'angular2021';
+  nombre = 'Ricardo';
+  apellido = 'Zambrano';
 }
